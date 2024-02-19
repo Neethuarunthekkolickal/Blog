8e97de49e878526c68a91cdc0667c0bdd7e84728
Neethu <neethuarunthekkolickal19@gmail.com>
2024-02-19 16:37:25 +0530

    delete and edit button added

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns =[
     path("",views.index,name="index"),
     path("post",views.post,name="post"),
-    path("singup",views.singup,name="singup")
+    path("singup",views.singup,name="singup"),
+    path("show/<int:id>",views.show,name="show"),
+    path("delete/<int:id>",views.delete,name="delete"),
+    path("edit/<int:id>",views.edit,name="edit")
 ]
--- app/views.py
+++ app/views.py
@@ -21,7 +21,6 @@ def post(request):
             form.save()
             form=MakePost()
             return render(request,"post.html",{"form":form})
-    return render(request,"post.html",{"form":form})
 def singup(request):
     form = UserCreationForm()
     if request.method == 'POST':
@@ -30,3 +29,24 @@ def singup(request):
             form.save()
             return redirect("singup")
     return render(request,"singup.html",{"form":form})
+def show(request,id):
+    post = Post.objects.get(id=id)
+    return render(request,"show.html",{"post":post})
+@login_required
+def delete(request,id):
+    post=Post.objects.get(id=id)
+    if request.method=="POST":
+        post.delete()
+        return redirect("index")
+@login_required    
+def edit(request,id):
+    post=Post.objects.get(id=id)
+    form=MakePost(instance=post)
+    if request.method=="POST":
+        form=MakePost(request.POST,request.FILES,instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    return render(request,"edit.html",{"form":form})
+   
+    
